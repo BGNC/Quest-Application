0410e8262e6a99ba95ea65842ed46f638d54af64
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-06-14 20:58:20 +0300

    LikeController.java / LikeService.java / LikeRepository.java is updated

--- src/main/java/com/bgnc/questapp/controller/LikeController.java
+++ src/main/java/com/bgnc/questapp/controller/LikeController.java
@@ -1,13 +1,49 @@
 package com.bgnc.questapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bgnc.questapp.model.Like;
+import com.bgnc.questapp.service.LikeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/likes")
 public class LikeController {
 
+    private LikeService likeService;
 
-    
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
 
+
+    @GetMapping
+    public List<Like> getAllLikes(@RequestParam Optional<Long> userId, Optional<Long> postId){
+        return likeService.getAllLikes(userId,postId);
+
+    }
+
+    @GetMapping("/{likeId}")
+    public Like getLikeById(@PathVariable Long likeId){
+        return likeService.getLikeById(likeId);
+    }
+
+    @PostMapping
+    public Like saveLike(@RequestBody Like like){
+        return likeService.addLike(like);
+
+    }
+
+    @PutMapping("/{likeId}")
+    public Like updateLike(@PathVariable Long likeId ,@RequestBody Like like){
+        return likeService.updateById(likeId,like);
+
+    }
+
+
+    @DeleteMapping("/{likeId}")
+    public void deleteById(@PathVariable Long likeId){
+        likeService.deleteById(likeId);
+    }
 }
--- src/main/java/com/bgnc/questapp/service/LikeService.java
+++ src/main/java/com/bgnc/questapp/service/LikeService.java
@@ -1,4 +1,74 @@
 package com.bgnc.questapp.service;
 
+import com.bgnc.questapp.model.Like;
+import com.bgnc.questapp.model.Post;
+import com.bgnc.questapp.model.User;
+import com.bgnc.questapp.repository.LikeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LikeService {
+
+    private final LikeRepository likeRepository;
+    private final UserService userService;
+    private final PostService postService;
+
+
+    public LikeService(LikeRepository likeRepository, UserService userService, PostService postService) {
+        this.likeRepository = likeRepository;
+        this.userService = userService;
+        this.postService = postService;
+    }
+
+
+
+
+    public List<Like> getAllLikes(Optional<Long> userId, Optional<Long> postId) {
+
+        if(userId.isPresent() && postId.isPresent()){
+            return likeRepository.findByUserIdAndPostId(userId.get(),postId.get());
+        }
+        else if(userId.isPresent()){
+            return likeRepository.findByUserId(userId.get());
+        }
+        else if(postId.isPresent()){
+            return likeRepository.findByPostId(postId.get());
+        }
+        else
+            return likeRepository.findAll();
+    }
+
+
+    public void deleteById(Long likeId) {
+        likeRepository.deleteById(likeId);
+    }
+
+    public Like addLike(Like like) {
+        User user = userService.getUserById(like.getUser().getId());
+        Post post = postService.getPostById(like.getPost().getId());
+
+        if(user!=null && post!=null){
+
+            Like saveToLike = new Like();
+            like.setId(like.getId());
+            like.setPost(post);
+            like.setUser(user);
+            likeRepository.save(saveToLike);
+            return saveToLike;
+        }
+        else
+            return null;
+
+    }
+
+    public Like getLikeById(Long likeId) {
+        return likeRepository.findById(likeId).orElse(null);
+    }
+
+    public Like updateById(Long likeId, Like like) {
+
+    }
 }
